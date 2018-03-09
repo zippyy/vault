9803a7b3abcb630671399745123d70ec8f9b2959
tyrannosaurus-becks <beccapetrin@gmail.com>
2018-03-08 16:13:55 -0800

    begin documentation

--- builtin/logical/ldap/backend.go
+++ builtin/logical/ldap/backend.go
@@ -10,6 +10,10 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 
 	confHandler := newConfigurationHandler()
 
+	// TODO rotating a password is a requirement
+	// In PKI, the way you rotate a CRL is this:
+	// GET 	/pki/crl/rotate 	200 application/json
+	// So maybe it would be similar to that
 	return &framework.Backend{
 		Help: "TODO",
 		Paths: []*framework.Path{
